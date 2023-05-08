b8cc5817bb48455424745849b7754e0fcb0f4d85
anurupakopalli <132798250+anurupakopalli@users.noreply.github.com>
2023-05-07 22:04:34 -0500

    Add files via upload

---
+++ blog.Rmd.R
@@ -0,0 +1,11 @@
+# Exploratory Data Analysis
+
+In this blog post, I will be exploring the "fashionblogs" dataset in R. This dataset contains information about various fashion models
+
+## Loading the dataset
+
+We can load the dataset using the following code:
+  
+  ```{r}
+library(datasets)
+data(fashionblogs)
